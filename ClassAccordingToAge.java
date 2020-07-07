e6b259fb9561bd5934eb06c18ebbc63fbaf36a55
FeiBaliou <48136578+FeiBaliou@users.noreply.github.com>
2020-07-07 18:39:56 +0300

    Update ClassAccordingToAge.java

--- ClassAccordingToAge.java
+++ ClassAccordingToAge.java
@@ -12,24 +12,13 @@ System.out.println("Enter your age: ");
 byte age = in.nextByte();
 
 switch (age) {
- case 5:
- case 6:
- case 7:
- case 8:
- case 9:
- case 10:
- case 11:
- case 12:
+ case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12:
  System.out.println("You are going to Elementary School.");
  break;
- case 13:
- case 14:
- case 15:
+ case 13: case 14: case 15:
  System.out.println("You are going to Middle School.");
  break;
- case 16:
- case 17:
- case 18:
+ case 16: case 17: case 18:
  System.out.println("You are going to High School.");
  break;
  default:
