242e069743a98603d0093be37fa6b01813d79bc8
FeiBaliou <48136578+FeiBaliou@users.noreply.github.com>
2020-07-08 06:44:35 +0300

    Update IntegerInAnArray.java

--- IntegerInAnArray.java
+++ IntegerInAnArray.java
@@ -46,10 +46,7 @@ for(int i=1; i<numbers.length;i++){
 System.out.println("The smallest number is : "+min + ".");
 
 
-//int avg = numbers[0];
-//for (int i=1; i<numbers.length; i++){
-//sum += (numbers[i]);
-//    }
+
       float avg = sum / numbers.length;
       System.out.println("Average: " + avg);
 
