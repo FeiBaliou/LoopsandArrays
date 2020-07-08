a64d2ce4f5105444d59b18d17b1314d76802034a
FeiBaliou <48136578+FeiBaliou@users.noreply.github.com>
2020-07-08 07:14:32 +0300

    Add files via upload

---
+++ IntegerToDay.java
@@ -0,0 +1,52 @@
+/*8. Create a method that accepts an integer and returns the day of the week (Advanced). */
+
+
+import java.util.Scanner;
+
+public class IntegerToDay {
+
+public static void main (String [] args) {
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Please enter a number from 1 to 7: ");
+int number = in.nextInt();
+
+returnDay(in,number);
+
+}
+
+
+public static void returnDay(Scanner in, int number){
+
+switch (number) {
+case 1 :
+System.out.println("Monday");
+break;
+case 2 :
+System.out.println("Tuesday");
+break;
+case 3 :
+System.out.println("Wednesday");
+break;
+case 4 :
+System.out.println("Thursday");
+break;
+case 5 :
+System.out.println("Friday");
+break;
+case 6 :
+System.out.println("Saturday");
+break;
+case 7 :
+System.out.println("Sunday");
+break;
+default :
+System.out.println("Wrong input");
+}
+
+
+}
+
+
+}
